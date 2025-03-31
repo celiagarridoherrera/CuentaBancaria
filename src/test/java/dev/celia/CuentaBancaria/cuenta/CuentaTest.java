e290afe49215380087196f2c4e898774015b5fa9
Celia Garrido <celiagarridoherrera@gmail.com>
2025-03-31 22:40:00 +0200

    Creado testConsignarIncrementoSaldoyNumConsignaciones, test falla

--- src/test/java/dev/celia/CuentaBancaria/cuenta/CuentaTest.java
+++ src/test/java/dev/celia/CuentaBancaria/cuenta/CuentaTest.java
@@ -13,4 +13,12 @@ public class CuentaTest {
         assertEquals(0.05f, cuenta.getTasaAnual(), 0.001);
     }
 
+    @Test
+    void testConsignarIncrementoSaldoyNumConsignaciones(){
+        Cuenta cuenta = new Cuenta(5000.0f, 0.05f);
+        cuenta.consignar(1000.0f);
+        assertEquals(6000.0f, cuenta.getSaldo(), 0.001);
+        assertEquals(1, cuenta.getNumConsignaciones());
+    }
+
 }
