6110a4caf8c5bfeeef3db50ce1bc73a2d0a4cf6c
Celia Garrido <celiagarridoherrera@gmail.com>
2025-03-31 23:17:11 +0200

    Creados los test para comisionmensualextra e imprimirdatos, no corren

--- src/test/java/dev/celia/CuentaBancaria/cuenta/CuentaAhorrosTest.java
+++ src/test/java/dev/celia/CuentaBancaria/cuenta/CuentaAhorrosTest.java
@@ -1,5 +1,7 @@
 package dev.celia.CuentaBancaria.cuenta;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CuentaAhorrosTest {
 
     @Test
@@ -22,4 +24,26 @@ public class CuentaAhorrosTest {
         assertEquals(9000.0f, cuenta.getSaldo(), 0.001);
     }
 
+    @Test
+    void testComisionMensualExtra(){
+        CuentaAhorros cuenta = new CuentaAhorros(15000.0f, 0.12f);
+        for (int i = 0; i < 6; i++) {
+            cuenta.retirar(1000.0f);
+        }
+        cuenta.calcularComisionMensual();
+        assertTrue(cuenta.getSaldo() < 15000.0f);
+        assertEquals(2000.0f, cuenta.comisionMensual,0.001);
+    }
+
+    @Test
+    void testImprimirDatos(){
+        CuentaAhorros cuenta = new CuentaAhorros(15000.0f, 0.05f);
+        cuenta.consignar(500.0f);
+        cuenta.retirar(200.0f);
+    
+
+        String datos = cuenta.imprimir();
+        assertTrue(datos.contains("Saldo:"));
+        assertTrue(datos.contains("Transacciones:"));
+    }
 }
