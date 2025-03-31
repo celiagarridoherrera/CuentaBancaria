488118e59940b6c3a91685dba6135bf0de578446
Celia Garrido <celiagarridoherrera@gmail.com>
2025-03-31 23:08:08 +0200

    Test testCuentaInactivaNoPuedeConsignar, falla

--- src/test/java/dev/celia/CuentaBancaria/cuenta/CuentaAhorrosTest.java
+++ src/test/java/dev/celia/CuentaBancaria/cuenta/CuentaAhorrosTest.java
@@ -8,4 +8,11 @@ public class CuentaAhorrosTest {
         assertFalse(cuenta.isActiva());
     }
 
+    @Test
+    void testCuentaInactivaNoPuedeConsignar(){
+        CuentaAhorros cuenta = new CuentaAhorros(9000.0f, 0.04f);
+        cuenta.consignar(1000.0f);
+        assertEquals(9000.0f, cuenta.getSaldo(), 0.001);
+    }
+
 }
