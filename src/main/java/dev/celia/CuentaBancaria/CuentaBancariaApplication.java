2627ca95f031a6a2ed4ae74f76673884a56bbe15
Celia Garrido <celiagarridoherrera@gmail.com>
2025-03-31 22:24:41 +0200

    first commit

---
+++ src/main/java/dev/celia/CuentaBancaria/CuentaBancariaApplication.java
@@ -0,0 +1,13 @@
+package dev.celia.CuentaBancaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuentaBancariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuentaBancariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/celia/CuentaBancaria/CuentaBancariaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.celia.CuentaBancaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuentaBancariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
