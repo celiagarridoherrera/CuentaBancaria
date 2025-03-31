9d2990a17f08158ba2a090b37cbeb22091e34abc
Celia Garrido <celiagarridoherrera@gmail.com>
2025-03-31 22:36:31 +0200

    Clase cuenta creada, los test corren perfectamente - fix: añadido un import en CuentaTest que faltaba

---
+++ src/main/java/dev/celia/CuentaBancaria/cuenta/Cuenta.java
@@ -0,0 +1,19 @@
+package dev.celia.CuentaBancaria.cuenta;
+
+public class Cuenta {
+    protected Float saldo;
+    protected Integer numConsignaciones = 0;
+    protected Integer numRetiros = 0;
+    protected Float tasaAnual;
+    protected Float comisionMensual = 0.0f;
+    public Cuenta(Float saldo, Float tasaAnual) {
+        this.saldo = saldo;
+        this.tasaAnual = tasaAnual;
+    }
+    public Float getSaldo() {
+        return saldo;
+    }
+    public Float getTasaAnual() {
+        return tasaAnual;
+    }
+}
